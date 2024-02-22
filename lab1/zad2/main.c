29bcff308cb271dd850ed8c386559f45cd634dae
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2024-02-22 18:08:47 +0100

    lab1

---
+++ lab1/zad2/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float liczba, liczba2 ;
+    scanf("%f", &liczba);
+    scanf("%f", &liczba2);
+    printf("roznica = %f\n", liczba-liczba2);
+    return 0;
+}
