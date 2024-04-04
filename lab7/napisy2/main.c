e1c4c953d7240c122c8fa9cbba1d1c4d1990bb9f
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2024-04-04 18:08:21 +0200

    napis2

---
+++ lab7/napisy2/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int countNums(char str[]){
+    int counter=0;
+    for(int i=0; str[i] !=0;i++){
+        if (str[i] >= '0' && str[i] <= '9'){
+            counter++;
+        }
+    }
+    return counter;
+}
+
+int main()
+{
+    char t1[] = "ashshdghe3\000234ds";
+    printf("%s\n", t1);
+    printf("%d\n", countNums(t1));
+    return 0;
+}
